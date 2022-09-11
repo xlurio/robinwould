70777cd6ad11dd14785e869ee964f305065bb95f
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-11 19:20:41 -0300

    fix: removed trailing spaces

--- robinwould/__init__.py
+++ robinwould/__init__.py
@@ -1,5 +1,5 @@
-"""RobinWould is framework for fast and easy development on web scraping tools based. 
-With less than 10 lines of code you already have script ready to fish for data on the 
+"""RobinWould is framework for fast and easy development on web scraping tools based.
+With less than 10 lines of code you already have script ready to fish for data on the
 web."""
 
 __version__ = "0.1.0"
