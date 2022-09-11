b4e1f04ee67960c180600ba37787e89e7a999ded
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-11 19:11:34 -0300

    docs: package description updated

--- robinwould/__init__.py
+++ robinwould/__init__.py
@@ -1,4 +1,6 @@
-"""Python framework for fast development of scraping tools"""
+"""RobinWould is framework for fast and easy development on web scraping tools based. 
+With less than 10 lines of code you already have script ready to fish for data on the 
+web."""
 
 __version__ = "0.1.0"
 
